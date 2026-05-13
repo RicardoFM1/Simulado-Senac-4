75fde7b7e9b9516bb38dcb9c78317aa43d7152da
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-05-13 15:29:35 -0300

    feat(backend usuario): Endpoint de usuário pronto.

---
+++ Backend/Controllers/Usuario/usuarioController.php
@@ -0,0 +1,142 @@
+<?php
+require_once __DIR__ . "/../../Services/Usuario/usuarioService.php";
+require_once __DIR__ . "/../../Middleware/middleware.php";
+
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+
+
+class UsuarioController
+{
+    protected $usuarioService;
+    protected $chaveSecreta;
+
+    public function __construct()
+    {
+        $this->usuarioService = new UsuarioService();
+        $this->chaveSecreta = $_ENV['JWT_SECRET_KEY'];
+    }
+
+    public function validarDados($dados)
+    {
+        try {
+            $cargosPermitidos = ['admin', 'ceremonialista'];
+            $esquema = v::key('nome', v::stringVal()->notEmpty()->length(1, 45))
+                ->key('email', v::email())
+                ->key('cpf', v::cpf())
+                ->key('senha', v::stringVal()->notEmpty()->length(8, 255))
+                ->key('cargo', v::in($cargosPermitidos));
+
+            $esquema->assert($dados);
+        } catch (NestedValidationException $e) {
+            $mensagemPersonalizada = [
+                'nome' => 'Nome inválido, min 1, max 45',
+                'email' => 'Email inválido',
+                'cpf' => 'Cpf inválido',
+                'senha' => 'Senha inválida, min 8, max 255',
+                'cargo' => 'Cargo fora do escopo: admin ou ceremonialista apenas'
+            ];
+
+            $mensagemOriginal = $e->getMessages();
+            $mensagemTraduzida = [];
+
+            foreach ($mensagemOriginal as $campo => $mensagem) {
+                $mensagemTraduzida[$campo] = $mensagemPersonalizada[$campo] ?? $mensagem;
+            }
+
+            echo json_encode([
+                'sucesso' => false,
+                'mensagem' => 'Erros de validação',
+                'erros' => $mensagemTraduzida
+            ]);
+            http_response_code(400);
+            exit;
+        }
+    }
+
+    public function apenasAdmin()
+    {
+        $jwt = Middleware::validarMiddleware();
+
+        if ($jwt->dados->cargo_usuario !== 'admin') {
+            http_response_code(401);
+            echo json_encode([
+                'sucesso' => false,
+                'mensagem' => 'Usuário sem permissão'
+            ]);
+            exit;
+        }
+    }
+
+    public function listarUsuarios()
+    {
+        $this->apenasAdmin();
+        http_response_code(200);
+        echo json_encode($this->usuarioService->listarUsuarios());
+        exit;
+    }
+
+    public function criarUsuario()
+    {
+        try {
+            $this->apenasAdmin();
+            $dados = json_decode(file_get_contents('php://input'), true);
+            $this->validarDados($dados);
+
+            echo json_encode($this->usuarioService->criarUsuario($dados));
+            exit;
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            echo json_encode([
+                'sucesso' => false,
+                'mensagem' => $e->getMessage()
+            ]);
+            exit;
+        }
+    }
+
+    public function fazerLogin()
+    {
+        $dados = json_decode(file_get_contents('php://input'), true);
+        echo json_encode($this->usuarioService->fazerLogin($dados, $this->chaveSecreta));
+        exit;
+    }
+
+    public function atualizarUsuario()
+    {
+        try {
+            $this->apenasAdmin();
+            $dados = json_decode(file_get_contents('php://input'), true);
+            $this->validarDados($dados);
+            $emailUsuario = $_GET['email_usuario'];
+            echo json_encode($this->usuarioService->atualizarUsuario($dados, $emailUsuario));
+            exit;
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            echo json_encode([
+                'sucesso' => false,
+                'mensagem' => $e->getMessage()
+            ]);
+            exit;
+        }
+    }
+
+    public function deletarUsuario()
+    {
+        try {
+            $this->apenasAdmin();
+
+
+            $emailUsuario = $_GET['email_usuario'];
+            echo json_encode($this->usuarioService->deletarUsuario($emailUsuario));
+            exit;
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            echo json_encode([
+                'sucesso' => false,
+                'mensagem' => $e->getMessage()
+            ]);
+            exit;
+        }
+    }
+}
---
+++ Backend/Routes/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . "/..//vendor/autoload.php";
+require_once __DIR__ . "/../Controllers/Usuario/usuarioController.php";
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+$rota = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Authorization, Content-Type');
+header('Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Credentials: true');
+
+if ($metodo === 'OPTIONS') {
+    http_response_code(200);
+}
+
+
+if ($rota === '/usuario') {
+    $usuarioController = new UsuarioController();
+
+    if ($metodo === 'GET') {
+        $usuarioController->listarUsuarios();
+    }
+    if ($metodo === 'POST') {
+        $usuarioController->criarUsuario();
+    }
+    if ($metodo === 'PUT') {
+        $usuarioController->atualizarUsuario();
+    }
+    if ($metodo === 'DELETE') {
+        $usuarioController->deletarUsuario();
+    }
+}
+
+if ($rota === '/usuario/login') {
+    $usuarioController = new UsuarioController();
+
+
+    if ($metodo === 'POST') {
+        $usuarioController->fazerLogin();
+    }
+}
